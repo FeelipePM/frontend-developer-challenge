96a1ec52b0a38c34e545a01e6dd66f462f755ea0
Felipe Machado <feelipe@email.com>
2019-06-23 19:16:31 -0300

    Renderizamos os produtos na tela 1 à 1

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,29 @@
 (function() {
   const $root = document.querySelector("#root");
 
+  const $product = product.render(
+    "",
+    "Nome do produto",
+    "Descrição do produto um pouco maior.com duas linhas ou três que explica melhor do que se trata.",
+    "23,99",
+    "19,99"
+  );
+
+  const $productWrapper = productWrapper.render(
+    $product,
+    $product,
+    $product,
+    $product,
+    $product,
+    $product,
+    $product,
+    $product
+  );
+
   const $headerContent = headerContent.render();
 
   const $headerWrapper = headerWrapper.render($headerContent);
 
   $root.insertAdjacentHTML("beforeend", $headerWrapper);
+  $root.insertAdjacentHTML("beforeend", $productWrapper);
 })();
