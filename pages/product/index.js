2de4a31980d785faefd1a3565c78501b13df4802
Felipe Machado <feelipe@email.com>
2019-06-23 19:49:26 -0300

    renderizado o footer dentro da para para poder ser executado.

--- pages/product/index.js
+++ pages/product/index.js
@@ -23,12 +23,15 @@
   const $headerContent = headerContent.render();
   const $moreProducts = moreProducts.render();
   const $newsletterContent = newsletterContent.render();
+  const $footerTitle = footerTitle.render();
 
   const $headerWrapper = headerWrapper.render($headerContent);
   const $newsletterWrapper = newsletterWrapper.render($newsletterContent);
+  const $footerWrapper = footerWrapper.render($footerTitle);
 
   $root.insertAdjacentHTML("beforeend", $headerWrapper);
   $root.insertAdjacentHTML("beforeend", $productWrapper);
   $root.insertAdjacentHTML("beforeend", $moreProducts);
   $root.insertAdjacentHTML("beforeend", $newsletterWrapper);
+  $root.insertAdjacentHTML("beforeend", $footerWrapper);
 })();
